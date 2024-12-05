ba252c0b10db87cf646e02b5d92d907fe9117f70
frankiesochan <jjaviendemonteverde@gmail.com>
2024-12-05 10:28:10 +0800

    added change password route

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -7,5 +7,6 @@ router.post('/follow-user/:userId/:followId', UserController.followUser)
 router.delete('/unfollow-user/:userId/:unfollowId', UserController.unfollowUser)
 router.post('/add-bio/:userId', UserController.addBio)
 router.post('/add-link/:userId', UserController.addLink)
+router.put('/change-password/:userId', UserController.changePassword)
 
 module.exports = router
