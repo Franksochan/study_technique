b727d100a37061ce6532bc95bbf1492749f0f5fd
frankiesochan <jjaviendemonteverde@gmail.com>
2024-12-07 06:59:48 +0800

    added application route

--- server/index.js
+++ server/index.js
@@ -8,13 +8,15 @@ const logger = require('./logger/logger')
 const authRoute = require('./router/authRoute')
 const userRoute = require('./router/userRouter')
 const jobRoute = require('./router/jobRoute')
+const applicationRoute = require('./router/applicationRoute')
 const errorHandler = require('./middleware/errorHandler')
 
 const app = express();
 const port = process.env.PORT || 5000
 
-// To handle JSON requests
-app.use(express.json())
+// Configure Express to parse JSON requests with a maximum size limit of 50mb
+app.use(express.json({ limit: '100mb' }))
+app.use(express.urlencoded({ extended: true, limit: '100mb', parameterLimit: 1000000 }))
 
 const corsOptions = {
   origin: "http://localhost:5173", // replace with your frontend URL
@@ -28,6 +30,7 @@ app.use(cors(corsOptions))
 app.use('/auth', authRoute)
 app.use('/user', userRoute)
 app.use('/job', jobRoute)
+app.use('/application', applicationRoute)
 
 app.use(errorHandler)
  
