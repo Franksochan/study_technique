c29e6c3f97f6c9361e082517eac1c2ca898de430
frankiesochan <jjaviendemonteverde@gmail.com>
2024-12-07 12:40:19 +0800

    added useUserData hook

---
+++ client/hooks/useUserData.js
@@ -0,0 +1,34 @@
+import { useState, useEffect, memo } from "react"
+import usePrivateApi from "./usePrivateApi"
+import { useNavigate } from "react-router-dom"
+
+const useUserData = () => {
+  const [user, setUser] = useState({})
+  const userID = localStorage.getItem('userID')
+  const navigate = useNavigate()
+  const privateAxios = usePrivateApi()
+
+  useEffect(() => {
+    console.log(userID)
+    const fetchUserData = async () => {
+      try {
+        const response = await privateAxios.get(
+          `/user/get-user/${userID}`
+        )
+        console.log(response.data.user)
+        setUser(response.data.user)
+      } catch (error) {
+        console.log(error)
+        if (error.response && (error.response.status === 401 || error.response.status === 403 || error.response.status === 404 || error.response.status === 500 || error.response.status === 400)) {
+          console.log("Token expired. Navigating to /auth")
+          navigate('/auth')
+        }
+      }
+    }
+    fetchUserData()
+  }, [])
+
+  return { user }
+}
+
+export default useUserData
