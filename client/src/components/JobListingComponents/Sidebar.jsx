9f9a555fe9903c9dd34d77a57d63bf1190c443f3
frankiesochan <jjaviendemonteverde@gmail.com>
2024-12-08 20:56:32 +0800

    added user data and tweak some css to fix design

--- client/src/components/JobListingComponents/Sidebar.jsx
+++ client/src/components/JobListingComponents/Sidebar.jsx
@@ -1,24 +1,28 @@
+import { useEffect, useState } from 'react'
+import useUserData from '../../../hooks/useUserData'
 import './Sidebar.css'
 
 const Sidebar = () => {
+  const { user } = useUserData()
+
   const userId = localStorage.getItem('userID')
   return (
     <aside className="sidebar">
       <div className="profile-section">
         <img 
-          src="https://via.placeholder.com/80" 
+          src={ user?.profilePic ? user?.profilePic : "https://via.placeholder.com/80" }
           alt="Profile" 
           className="profile-icon"
         />
-        <h3 className="profile-name">John Doe</h3>
+        <h3 className="profile-name">{user.name}</h3>
       </div>
       <nav className="sidebar-menu">
         <ul>
           <li><a href='/profile'>Profile</a></li>
           <li><a href='/dashboard'>Dashboard</a></li>
           <li><a href='/settings'>Account Settings</a></li>
-          <li><a href='/messages'>Messages</a></li>
           <li><a href={`/notifications/${userId}`}>Notifications</a></li>
+          <li><a href='/login'>Logout</a></li>
         </ul>
       </nav>
     </aside>
