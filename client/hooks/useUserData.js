92c1f8cbbc62f8afb28649b6acccc44bb541bcac
frankiesochan <jjaviendemonteverde@gmail.com>
2024-12-15 16:35:21 +0800

    fixed some bugs

--- client/hooks/useUserData.js
+++ client/hooks/useUserData.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, memo } from "react"
+import { useState, useEffect } from "react"
 import usePrivateApi from "./usePrivateApi"
 import { useNavigate } from "react-router-dom"
 
@@ -15,18 +15,20 @@ const useUserData = () => {
         const response = await privateAxios.get(
           `/user/get-user/${userID}`
         )
-        console.log(response.data.user)
         setUser(response.data.user)
       } catch (error) {
         console.log(error)
         if (error.response && (error.response.status === 401 || error.response.status === 403 || error.response.status === 404 || error.response.status === 500 || error.response.status === 400)) {
-          console.log("Token expired. Navigating to /auth")
-          navigate('/auth')
+          console.log("Token expired. Navigating to /login")
+          navigate('/login')
         }
       }
     }
-    fetchUserData()
-  }, [])
+
+    if (userID) { 
+      fetchUserData()
+    }
+  }, [userID])
 
   return { user }
 }
