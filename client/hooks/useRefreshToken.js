11fa719d2e1c78028fadab3627a9f2189b01e3ad
frankiesochan <jjaviendemonteverde@gmail.com>
2024-12-15 16:35:02 +0800

    fixed the throwing of error

--- client/hooks/useRefreshToken.js
+++ client/hooks/useRefreshToken.js
@@ -11,13 +11,14 @@ const useTokenRefresh = () => {
         { withCredentials: true }
       )
 
-      const newAccessToken = refreshResponse.data.newAccessToken
+      const newAccessToken = refreshResponse.data.accessToken
 
       setAuth({ accessToken: newAccessToken })
 
       return newAccessToken
     } catch (refreshError) {
       console.error("Error refreshing token:", refreshError)
+      throw refreshError
     }
   }
 
