b9662738ccb67ecb213e6b20b4024ffe426074cc
frankiesochan <jjaviendemonteverde@gmail.com>
2024-12-15 16:41:14 +0800

    added verifyToken middleware tpo routes

--- server/router/applicationRoute.js
+++ server/router/applicationRoute.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const ApplicationController = require('../controller/ApplicationController')
 const { upload } = require('../middleware/multer')
+const { verifyToken } = require('../middleware/jsonWebTokens')
 
-router.post('/apply-job/:jobId/:applicantId', upload, ApplicationController.applyJob)
-router.post('/offer-job/:jobId/:applicantId', ApplicationController.offerJob)
+router.post('/apply-job/:jobId/:applicantId', verifyToken, upload, ApplicationController.applyJob)
+router.post('/offer-job/:jobId/:applicantId', verifyToken, ApplicationController.offerJob)
 
 module.exports = router
--- server/router/jobRoute.js
+++ server/router/jobRoute.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const JobController = require('../controller/JobController')
+const { verifyToken } = require('../middleware/jsonWebTokens')
 
-router.post('/create-job/:userId', JobController.createJob)
-router.get('/get-jobs/:jobCategory', JobController.fetchJobs)
-router.delete('/delete-job/:userId/:jobId', JobController.deleteJobs)
-router.get('/get-job-details/:jobId', JobController.getJobDetails)
-router.get('/get-job-applicants/:jobId', JobController.getJobApplicants)
+router.post('/create-job/:userId', verifyToken, JobController.createJob)
+router.get('/get-jobs/:jobCategory', verifyToken, JobController.fetchJobs)
+router.delete('/delete-job/:userId/:jobId', verifyToken, JobController.deleteJobs)
+router.get('/get-job-details/:jobId', verifyToken, JobController.getJobDetails)
+router.get('/get-job-applicants/:jobId', verifyToken, JobController.getJobApplicants)
 
 module.exports = router
