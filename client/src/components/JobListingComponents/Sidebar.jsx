8cd89f38fa8bc8fc434872f23010a78a16c6add3
frankiesochan <jjaviendemonteverde@gmail.com>
2024-12-15 16:42:55 +0800

    fixed sidebaar by chaning href to link

--- client/src/components/JobListingComponents/Sidebar.jsx
+++ client/src/components/JobListingComponents/Sidebar.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaUser, FaCog, FaBell, FaSuitcase, FaPaperclip, FaSignOutAlt } from 'react-icons/fa'
 import useUserData from '../../../hooks/useUserData'
 import './Sidebar.css'
 
@@ -15,21 +16,21 @@ const Sidebar = ({ showSidebar, setShowSidebar }) => {
     <aside className={`sidebar ${showSidebar ? 'expanded' : 'collapsed'}`}>
       <div className="profile-section" onClick={handleProfileClick}>
         <img 
-          src={user?.profilePic ? user?.profilePic : "empty dp.jpg"}
+          src={user?.profilePic ? user?.profilePic : "/empty dp.jpg"}
           alt="Profile" 
           className="profile-icon"
         />
-        {showSidebar && <h3 className="profile-name">{user.name}</h3>}  {/* Show name only when expanded */}
+        {showSidebar && <h3 className="profile-name">{user.name}</h3>} 
       </div>
       {showSidebar && (  
         <nav className="sidebar-menu">
           <ul>
-            <li><a href='/profile'>Profile</a></li>
-            <li><a href='/settings'>Account Settings</a></li>
-            <li><a href={`/notifications/${userId}`}>Notifications</a></li>
-            <li><a href={`/applied-jobs/${userId}`}>Applied Jobs</a></li>
-            <li><a href={`/posted-jobs/${userId}`}>Posted Jobs</a></li>
-            <li><a href='/login'>Logout</a></li>
+            <li><Link to='/profile'><FaUser /> Profile</Link></li>
+            <li><Link to='/settings'><FaCog /> Account Settings</Link></li>
+            <li><Link to={`/notifications/${userId}`}><FaBell /> Notifications</Link></li>
+            <li><Link to={`/applied-jobs/${userId}`}><FaSuitcase /> Applied Jobs</Link></li>
+            <li><Link to={`/posted-jobs/${userId}`}><FaPaperclip /> Posted Jobs</Link></li>
+            <li><Link to='/login'><FaSignOutAlt /> Logout</Link></li>
           </ul>
         </nav>
       )}
