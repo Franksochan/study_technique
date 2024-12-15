c4da2fb0751d96fda3a3ebb292fbb7b8b94317fa
frankiesochan <jjaviendemonteverde@gmail.com>
2024-12-15 16:39:22 +0800

    added new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Suspense, lazy } from 'react'
+import { AuthProvider } from '../context/AuthContext'
 import './App.css'
 
 const Login = lazy(() => import('./pages/login/Login'))
@@ -14,27 +15,38 @@ const JobApplicants = lazy(() => import('./pages/jobApplicants/JobApplicants'))
 const NotificationsPage = lazy(() => import('./pages/notifications/Notifications'))
 const ApplicantProfilePage = lazy(() => import('./pages/applicantProfile/ApplicantProfilePage'))
 const AppliedJobsPage = lazy(() => import('./pages/appliedJobs/AppliedJobs'))
+const PersistLogin = lazy(() => import('./components/PersistLogin'))
+const PostedJobsPage = lazy(() => import('./pages/postedJobs/PostedJobs'))
+const JobPosterProfile = lazy(() => import('./pages/jobPosterProfile/JobPosterProfile'))
+const HamsterWheel = lazy(() => import('./components/HamsterWheel/HamsterWheel'))
 
 const App = () => {
   return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/login' element={ <Login /> } />
-          <Route path='/' element={ <LandingPage/> } />
-          <Route path='/register' element={ <Registration/> } />
-          <Route path='/verify-email/:email' element={ <Verification/> } />
-          <Route path='/job-listings' element={ <JobListingPage/> } />
-          <Route path='/profile' element={ <ProfilePage/> } />
-          <Route path='/settings' element={ <AccountSettings /> } />
-          <Route path='/apply/:jobId' element={ <JobApplicationPage />} />
-          <Route path='/applicants/:jobId' element={ <JobApplicants />} />
-          <Route path='/notifications/:userId' element={ <NotificationsPage />} />
-          <Route path='/applicant-profile-page/:applicantId/:jobId' element={ <ApplicantProfilePage />} />
-          <Route path='/applied-jobs/:userId' element={ <AppliedJobsPage />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='/login' element={ <Login /> } />
+            <Route path='/' element={ <LandingPage/> } />
+            <Route path='/register' element={ <Registration/> } />
+            <Route path='/verify-email/:email' element={ <Verification/> } />
+            <Route path='/hamster-wheel' element={ <HamsterWheel/>} />
+            <Route element={<PersistLogin />}>
+              <Route path='/job-listings' element={ <JobListingPage/> } />
+              <Route path='/profile' element={ <ProfilePage/> } />
+              <Route path='/settings' element={ <AccountSettings /> } />
+              <Route path='/apply/:jobId' element={ <JobApplicationPage />} />
+              <Route path='/applicants/:jobId' element={ <JobApplicants />} />
+              <Route path='/notifications/:userId' element={ <NotificationsPage />} />
+              <Route path='/applicant-profile-page/:applicantId/:jobId' element={ <ApplicantProfilePage />} />
+              <Route path='/job-poster-profile/:jobPosterId/:jobId' element={ <JobPosterProfile />} />
+              <Route path='/applied-jobs/:userId' element={ <AppliedJobsPage />} />
+              <Route path='/posted-jobs/:userId' element={ <PostedJobsPage />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </Router>
+    </AuthProvider>
   )
 }
 
