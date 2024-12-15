4357c93b361bdd6cde3f1b6661e5f72afcc71fba
frankiesochan <jjaviendemonteverde@gmail.com>
2024-12-15 18:00:09 +0800

    fix cors origin

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.json({ limit: '100mb' }))
 app.use(express.urlencoded({ extended: true, limit: '100mb', parameterLimit: 1000000 }))
 
 const corsOptions = {
-  origin: "http://localhost:5173", // replace with your frontend URL
+  origin: process.env.CLIENT, // replace with your frontend URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
